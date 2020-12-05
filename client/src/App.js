8e247af2030aa9fee5fff35dffc7313a491c3006
Ksu <newksyuha@gmail.com>
2020-12-05 12:07:20 +0200

    WIP Add Create-react-App

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
+/* import Header from "./components/Header/Header"; */
 
 
 function App() {
     return (
         <React.StrictMode>
             <Router>
-                <Header />
+                {/* <Header /> */}
                 <Switch>
 
                 </Switch>
